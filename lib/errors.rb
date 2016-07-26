3acf3e8735128ec4a2fe78086dadb8b6f83514a4
Georgia Davis <georgiadavis@Georgias-MacBook-Pro.local>
2016-07-26 09:49:19 -0700

    tried to refactor formate_date and format_priority methods and error methods in  errors.rb

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,7 +1,20 @@
 module UdaciListErrors
-  class ErrorName < StandardError
-    # if something_bad_happens
+  class InvalidItemType < StandardError
+    # if invalid_type
     #   raise ModuleName::ErrorName, "Scanner Communication Error..."
     # end
   end
+
+  class IndexExceedsListSize < StandardError
+    # if exceed_list_size
+    #   raise ModuleName::ErrorName, "Scanner Communication Error..."
+    # end
+  end
+
+  class InvalidPriorityValue < StandardError
+    # if invalid_priority
+    #   raise ModuleName::ErrorName, "Scanner Communication Error..."
+    # end
+  end
+
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -7,16 +7,16 @@ class TodoItem
     @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_date
-    @due ? @due.strftime("%D") : "No due date"
-  end
-  def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
-  end
+  # def format_date
+  #   @due ? @due.strftime("%D") : "No due date"
+  # end
+  # def format_priority
+  #   value = " ⇧" if @priority == "high"
+  #   value = " ⇨" if @priority == "medium"
+  #   value = " ⇩" if @priority == "low"
+  #   value = "" if !@priority
+  #   return value
+  # end
   def details
     format_description(@description) + "due: " +
     format_date +
